488a55dcfe2fb3696fc776cbf7107e619213549d
Quernest <a.zhit4enko@gmail.com>
2024-01-01 18:20:44 +0200

    changed names

--- run.js
+++ run.js
@@ -6,19 +6,21 @@ async function writeFileSyncRecursive(filename, content = "") {
   await fs.writeFile(filename, content);
 }
 
-async function fetchExchangeRates() {
+async function fetchExchangeRates(
+  baseCurrency = "USD",
+  folderName = "exchange-rates"
+) {
+  const apiEndpoint = `https://open.er-api.com/v6/latest?base=${baseCurrency}`;
+
   try {
     const currentDate = new Date();
     const formattedDate = currentDate.toISOString().split("T")[0];
 
-    const response = await fetch("https://open.er-api.com/v6/latest");
+    const response = await fetch(apiEndpoint);
     const { rates } = await response.json();
 
-    const filePath = path.join(
-      __dirname,
-      "exchange-rates",
-      `${formattedDate}.json`
-    );
+    const filePath = path.join(__dirname, folderName, `${formattedDate}.json`);
+
     await writeFileSyncRecursive(filePath, JSON.stringify(rates, null, 2));
 
     console.log("Exchange rates saved to:", filePath);
