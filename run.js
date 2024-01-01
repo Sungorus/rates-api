b6e33bc8683142166bb2296e502e7ffa6073ff9f
Quernest <a.zhit4enko@gmail.com>
2024-01-01 18:24:53 +0200

    added prettier rule

--- run.js
+++ run.js
@@ -1,31 +1,25 @@
-const fs = require("fs").promises;
-const path = require("path");
+const fs = require('fs').promises;
+const path = require('path');
 
-async function writeFileSyncRecursive(filename, content = "") {
+async function writeFileSyncRecursive(filename, content = '') {
   await fs.mkdir(path.dirname(filename), { recursive: true });
   await fs.writeFile(filename, content);
 }
 
 async function fetchExchangeRates(
-  baseCurrency = "USD",
-  folderName = "exchange-rates"
+  baseCurrency = 'USD',
+  folderName = 'exchange-rates'
 ) {
   const apiEndpoint = `https://open.er-api.com/v6/latest?base=${baseCurrency}`;
 
   try {
     const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().split("T")[0];
-
+    const formattedDate = currentDate.toISOString().split('T')[0];
     const response = await fetch(apiEndpoint);
     const { rates } = await response.json();
-
     const filePath = path.join(__dirname, folderName, `${formattedDate}.json`);
-
     await writeFileSyncRecursive(filePath, JSON.stringify(rates, null, 2));
-
-    console.log("Exchange rates saved to:", filePath);
   } catch (error) {
-    console.error("Error making request:", error.message);
     process.exit(1);
   }
 }
